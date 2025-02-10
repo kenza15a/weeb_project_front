7eb801a8a26c7b47c3e2de8182c59f13ef0e0bc8
Kenza Filali <filali1991.univ@gmail.com>
2025-02-10 16:27:07 +0100

    front-  project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-[#9333EA] text-white p-4">
+      <h1 className="text-3xl">Hello, WEEB!</h1>
+    </div>
+  );
+}
+
+export default App;
