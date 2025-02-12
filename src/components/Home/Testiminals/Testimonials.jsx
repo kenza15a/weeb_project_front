65aaf778bfee8e14a2e21af74394b1f279aae9b7
Kenza Filali <filali1991.univ@gmail.com>
2025-02-12 16:39:35 +0100

    homepage/desktop testimonials

--- src/components/Home/Testiminals/Testimonials.jsx
+++ src/components/Home/Testiminals/Testimonials.jsx
@@ -3,8 +3,8 @@ import Slider from "./Slider";
 
 const Testimonials = ({ titleText, imagesArray, ...props }) => {
   return (
-    <section className="flex flex-col items-center gap-y-2">
-      <h2 className="text-4xl font-bold text-white">{titleText}</h2>
+    <section className="flex flex-col items-center gap-y-2 p-y-12 md:p-y-0 ">
+      <h2 className="text-4xl font-bold text-white md:ml-2 text-center">{titleText}</h2>
       <Slider imagesArray={imagesArray} />
     </section>
   );
